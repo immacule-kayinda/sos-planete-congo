a320a57fd1b1fcfc84c9e79dc3b516a345d994a7
Immaculé Kayinda <immakaymw@gmail.com>
2025-05-21 02:03:17 +0100

    feat: update dependencies and enhance form components
    - Add new dependencies including @hookform/resolvers, @radix-ui/react-label, and others for improved form handling and UI components.
    - Introduce Form and Label components to streamline form creation and management.
    - Implement Toaster component for better user notifications and feedback.
    - Update package.json and package-lock.json to reflect the new dependencies and versions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Nunito } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { Toaster } from "@/components/ui/sonner";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${nunito.className} ${montserrat.variable} antialiased`}
       >
         {children}
+        <Toaster />
       </body>
     </html>
   );
