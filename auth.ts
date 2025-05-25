d8769af29bc7438714211be29304f5484369c77f
Immaculé Kayinda <immakaymw@gmail.com>
2025-05-25 16:45:52 +0100

    feat: extend NextAuth user and session interfaces to include user ID
    - Added 'id' property to User and Session interfaces for better user identification.
    - Updated JWT interface to include 'id' for token management.
    - Enhanced JWT and session callbacks to handle the new 'id' property.

--- auth.ts
+++ auth.ts
@@ -10,10 +10,12 @@ import authConfig from "./auth.config";
 
 declare module "next-auth" {
   interface User {
+    id: string;
     role?: string;
   }
   interface Session {
     user: {
+      id: string;
       role?: string;
     } & DefaultSession["user"];
   }
@@ -21,6 +23,7 @@ declare module "next-auth" {
 
 declare module "next-auth/jwt" {
   interface JWT {
+    id: string;
     role?: string;
   }
 }
@@ -70,12 +73,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
     async jwt({ token, user }) {
       if (user) {
+        token.id = user.id;
         token.role = user.role;
       }
       return token;
     },
     async session({ session, token }: { session: Session; token: JWT }) {
       if (session.user) {
+        session.user.id = token.id;
         session.user.role = token.role;
       }
       return session;
