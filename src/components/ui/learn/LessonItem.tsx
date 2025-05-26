e6d0361f70ad6a919b728086a0d22ac9492825a4
Immaculé Kayinda <immakaymw@gmail.com>
2025-05-26 11:46:00 +0100

    feat: enhance LessonItem component with state management
    - Added state handling to the LessonItem component to render different lesson states: BLOCKED, CURRENT, and FINISHED.
    - Introduced CurrentLesson and FinishedLesson components for improved lesson display.

--- src/components/ui/learn/LessonItem.tsx
+++ src/components/ui/learn/LessonItem.tsx
@@ -1,13 +1,23 @@
+import Finished from "@/app/quizz/ui/Finished";
 import BlockedLesson from "./blockedLesson";
+import CurrentLesson from "./currentLesson";
+import FinishedLesson from "./finishedLesson";
 
 export default function LessonItem({
   title,
   subtitle,
   stars,
+  state = "BLOKED",
 }: {
   title: string;
   subtitle: string;
   stars: number;
+  state?: "BLOKED" | "CURRENT" | "FINISHED";
 }) {
-  return <BlockedLesson stars={stars} title={title} subtitle={subtitle} />;
+  if (state === "BLOKED")
+    return <BlockedLesson stars={stars} title={title} subtitle={subtitle} />;
+  if (state === "CURRENT")
+    return <CurrentLesson stars={stars} title={title} subtitle={subtitle} />;
+  if (state === "FINISHED")
+    return <FinishedLesson subtitle={subtitle} title={title} />;
 }
